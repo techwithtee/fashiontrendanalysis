c344eefca066644bed10e035abf8c0260104273c
AltheaZeta <86517586+AltheaZeta@users.noreply.github.com>
2023-09-23 23:27:44 +0100

    Updates to Project files

--- src/main/java/com/wileyedge/fashiontrendanalysis/SecurityConfig.java
+++ src/main/java/com/wileyedge/fashiontrendanalysis/SecurityConfig.java
@@ -1,4 +1,34 @@
 package com.wileyedge.fashiontrendanalysis;
 
-public class SecurityConfig {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/login", "/register").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().loginPage("/login")
+                .and()
+                .csrf().disable();
+    }
 }
--- src/main/java/com/wileyedge/fashiontrendanalysis/dao/UserDaoImpl.java
+++ src/main/java/com/wileyedge/fashiontrendanalysis/dao/UserDaoImpl.java
@@ -1,4 +1,39 @@
 package com.wileyedge.fashiontrendanalysis.dao;
 
-public class UserDaoImpl {
+import com.wileyedge.fashiontrendanalysis.model.User;
+
+public class UserDaoImpl implements UserDao {
+    /**
+     * @param username
+     * @return
+     */
+    @Override
+    public User findByUsername(String username) {
+        return null;
+    }
+
+    /**
+     * @param id
+     * @return
+     */
+    @Override
+    public User findById(Long id) {
+        return null;
+    }
+
+    /**
+     * @param user
+     */
+    @Override
+    public void save(User user) {
+
+    }
+
+    /**
+     * @param user
+     */
+    @Override
+    public void update(User user) {
+
+    }
 }
