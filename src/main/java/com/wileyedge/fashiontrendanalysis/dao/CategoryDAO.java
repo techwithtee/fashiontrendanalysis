28b5faeedd3e325bf5c90530da821100c4233d56
AltheaZeta <86517586+AltheaZeta@users.noreply.github.com>
2023-09-23 16:47:17 +0100

    Updates Category and Product

--- src/main/java/com/wileyedge/fashiontrendanalysis/dao/CategoryDAO.java
+++ src/main/java/com/wileyedge/fashiontrendanalysis/dao/CategoryDAO.java
@@ -1,4 +1,51 @@
 package com.wileyedge.fashiontrendanalysis.dao;
 
+import com.wileyedge.fashiontrendanalysis.model.Category;
+
+import java.util.List;
+
 public interface CategoryDAO {
+
+    /**
+     * Retrieves all categories from the database.
+     *
+     * @return a list of all fashion trend categories
+     */
+    List<Category> getAllCategories();
+
+    /**
+     * Retrieves a specific category by its ID.
+     *
+     * @param categoryId the ID of the category to retrieve
+     * @return the category with the specified ID, or null if not found
+     */
+    Category getCategoryById(Long categoryId);
+
+    /**
+     * Adds a new category to the database.
+     * This method is intended for admin usage.
+     *
+     * @param category the category to add
+     * @return the ID of the newly created category
+     */
+    Long addCategory(Category category);
+
+    /**
+     * Updates an existing category in the database by its ID.
+     * This method is intended for admin usage.
+     *
+     * @param categoryId the ID of the category to update
+     * @param category the category object with updated details
+     * @return true if the category was updated successfully, false otherwise
+     */
+    boolean updateCategory(Long categoryId, Category category);
+
+    /**
+     * Removes a specific category by its ID from the database.
+     * This method is intended for admin usage.
+     *
+     * @param categoryId the ID of the category to delete
+     * @return true if the category was deleted successfully, false otherwise
+     */
+    boolean deleteCategory(Long categoryId);
 }
--- src/main/java/com/wileyedge/fashiontrendanalysis/dao/CategoryDaoImpl.java
+++ src/main/java/com/wileyedge/fashiontrendanalysis/dao/CategoryDaoImpl.java
@@ -1,4 +1,68 @@
 package com.wileyedge.fashiontrendanalysis.dao;
 
-public class CategoryDaoImpl {
+import com.wileyedge.fashiontrendanalysis.model.Category;
+
+import java.util.List;
+
+public class CategoryDaoImpl implements CategoryDAO {
+
+    /**
+     * Retrieves all categories from the database.
+     *
+     * @return a list of all fashion trend categories, or an empty list if no categories exist
+     */
+    @Override
+    public List<Category> getAllCategories() {
+        // Implementation goes here
+        return null;
+    }
+
+    /**
+     * Retrieves a specific category by its ID.
+     *
+     * @param categoryId the ID of the category to retrieve
+     * @return the category with the specified ID, or null if not found
+     */
+    @Override
+    public Category getCategoryById(Long categoryId) {
+        // Implementation goes here
+        return null;
+    }
+
+    /**
+     * Adds a new category to the database.
+     *
+     * @param category the category to add
+     * @return the ID of the newly created category, or null if the addition failed
+     */
+    @Override
+    public Long addCategory(Category category) {
+        // Implementation goes here
+        return null;
+    }
+
+    /**
+     * Updates an existing category in the database by its ID.
+     *
+     * @param categoryId the ID of the category to update
+     * @param category the category object with updated details
+     * @return true if the category was updated successfully, false otherwise
+     */
+    @Override
+    public boolean updateCategory(Long categoryId, Category category) {
+        // Implementation goes here
+        return false;
+    }
+
+    /**
+     * Removes a specific category by its ID from the database.
+     *
+     * @param categoryId the ID of the category to delete
+     * @return true if the category was deleted successfully, false otherwise
+     */
+    @Override
+    public boolean deleteCategory(Long categoryId) {
+        // Implementation goes here
+        return false;
+    }
 }
