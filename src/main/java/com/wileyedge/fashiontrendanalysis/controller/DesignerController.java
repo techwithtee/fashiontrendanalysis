d4d20e0eefe2fd8ca62561c9d5194c4e5d5d8e95
AltheaZeta <86517586+AltheaZeta@users.noreply.github.com>
2023-09-27 13:12:18 +0100

    Updates to controller
    Documentation add and Endpoint tested on Postman

--- src/main/java/com/wileyedge/fashiontrendanalysis/controller/DesignerController.java
+++ src/main/java/com/wileyedge/fashiontrendanalysis/controller/DesignerController.java
@@ -22,12 +22,25 @@ public class DesignerController {
         this.designerService = designerService;
     }
 
+    /**
+     * Fetches all designers.
+     *
+     * @return A list of all designers.
+     * @apiEndpoint GET http://localhost:6363/api/designers
+     */
     @GetMapping
     public ResponseEntity<List<Designer>> getAllDesigners() {
         List<Designer> designers = designerService.getAllDesigners();
         return new ResponseEntity<>(designers, HttpStatus.OK);
     }
 
+    /**
+     * Fetches a designer by their ID.
+     *
+     * @param id The ID of the designer to fetch.
+     * @return The designer with the given ID, or 404 Not Found if not present.
+     * @apiEndpoint GET http://localhost:6363/api/designers/{id}
+     */
     @GetMapping("/{id}")
     public ResponseEntity<Designer> getDesignerById(@PathVariable Long id) {
         Designer designer = designerService.getDesignerById(id);
@@ -38,12 +51,27 @@ public class DesignerController {
         }
     }
 
+    /**
+     * Adds a new designer.
+     *
+     * @param designer The designer entity to add.
+     * @return The ID of the newly added designer.
+     * @apiEndpoint POST http://localhost:6363/api/designers
+     */
     @PostMapping
     public ResponseEntity<Long> addDesigner(@RequestBody Designer designer) {
         Long designerId = designerService.addDesigner(designer);
         return new ResponseEntity<>(designerId, HttpStatus.CREATED);
     }
 
+    /**
+     * Updates a designer by their ID.
+     *
+     * @param id The ID of the designer to update.
+     * @param designer The updated designer entity.
+     * @return A response indicating the success of the operation.
+     * @apiEndpoint PUT http://localhost:6363/api/designers/{id}
+     */
     @PutMapping("/{id}")
     public ResponseEntity<Void> updateDesigner(@PathVariable Long id, @RequestBody Designer designer) {
         boolean updated = designerService.updateDesigner(id, designer);
@@ -54,6 +82,13 @@ public class DesignerController {
         }
     }
 
+    /**
+     * Deletes a designer by their ID.
+     *
+     * @param id The ID of the designer to delete.
+     * @return A response indicating the success of the operation.
+     * @apiEndpoint DELETE http://localhost:6363/api/designers/{id}
+     */
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteDesigner(@PathVariable Long id) {
         boolean deleted = designerService.deleteDesigner(id);
@@ -64,12 +99,26 @@ public class DesignerController {
         }
     }
 
+    /**
+     * Fetches designers based on their location.
+     *
+     * @param location The location to filter designers by.
+     * @return A list of designers from the specified location.
+     * @apiEndpoint GET http://localhost:6363/api/designers/location/{location}
+     */
     @GetMapping("/location/{location}")
     public ResponseEntity<List<Designer>> getDesignersByLocation(@PathVariable String location) {
         List<Designer> designers = designerService.getDesignersByLocation(location);
         return new ResponseEntity<>(designers, HttpStatus.OK);
     }
 
+    /**
+     * Fetches the trend count for a designer.
+     *
+     * @param id The ID of the designer.
+     * @return The trend count for the specified designer.
+     * @apiEndpoint GET http://localhost:6363/api/designers/{id}/trendCount
+     */
     @GetMapping("/{id}/trendCount")
     public ResponseEntity<Integer> getDesignerTrendCount(@PathVariable Long id) {
         Integer trendCount = designerService.getDesignerTrendCount(id);
@@ -80,6 +129,13 @@ public class DesignerController {
         }
     }
 
+    /**
+     * Fetches the popularity score for a designer.
+     *
+     * @param id The ID of the designer.
+     * @return The popularity score for the specified designer.
+     * @apiEndpoint GET http://localhost:6363/api/designers/{id}/popularityScore
+     */
     @GetMapping("/{id}/popularityScore")
     public ResponseEntity<Integer> getDesignerPopularityScore(@PathVariable Long id) {
         Integer popularityScore = designerService.getDesignerPopularityScore(id);
@@ -90,6 +146,13 @@ public class DesignerController {
         }
     }
 
+    /**
+     * Fetches products associated with a specific designer.
+     *
+     * @param designerId The ID of the designer.
+     * @return A list of products for the specified designer.
+     * @apiEndpoint GET http://localhost:6363/api/designers/{designerId}/products
+     */
     @GetMapping("/{designerId}/products")
     public ResponseEntity<List<Product>> getProductsForDesigner(@PathVariable Long designerId) {
         // Logic to get products for a designer
--- src/main/java/com/wileyedge/fashiontrendanalysis/controller/TrendController.java
+++ src/main/java/com/wileyedge/fashiontrendanalysis/controller/TrendController.java
@@ -20,12 +20,25 @@ public class TrendController {
         this.trendService = trendService;
     }
 
+    /**
+     * Fetches all trends.
+     *
+     * @return a list of all trends
+     * @apiEndpoint GET http://localhost:6363/api/trends
+     */
     @GetMapping
     public ResponseEntity<List<Trend>> getAllTrends() {
         List<Trend> trends = trendService.getAllTrends();
         return new ResponseEntity<>(trends, HttpStatus.OK);
     }
 
+    /**
+     * Retrieves a specific trend by its ID.
+     *
+     * @param id the ID of the trend to retrieve
+     * @return the trend with the specified ID, or 404 if not found
+     * @apiEndpoint GET http://localhost:6363/api/trends/{id}
+     */
     @GetMapping("/{id}")
     public ResponseEntity<Trend> getTrendById(@PathVariable Long id) {
         Trend trend = trendService.getTrendById(id);
@@ -36,12 +49,27 @@ public class TrendController {
         }
     }
 
+    /**
+     * Adds a new trend.
+     *
+     * @param trend the trend to add
+     * @return the ID of the newly created trend
+     * @apiEndpoint POST http://localhost:6363/api/trends
+     */
     @PostMapping
     public ResponseEntity<Long> addTrend(@RequestBody Trend trend) {
         Long trendId = trendService.addTrend(trend);
         return new ResponseEntity<>(trendId, HttpStatus.CREATED);
     }
 
+    /**
+     * Updates a trend by its ID.
+     *
+     * @param id the ID of the trend to update
+     * @param trend the updated trend details
+     * @return a confirmation of the update
+     * @apiEndpoint PUT http://localhost:6363/api/trends/{id}
+     */
     @PutMapping("/{id}")
     public ResponseEntity<Void> updateTrend(@PathVariable Long id, @RequestBody Trend trend) {
         boolean updated = trendService.updateTrend(id, trend);
@@ -52,6 +80,13 @@ public class TrendController {
         }
     }
 
+    /**
+     * Deletes a trend by its ID.
+     *
+     * @param id the ID of the trend to delete
+     * @return a confirmation of the deletion
+     * @apiEndpoint DELETE http://localhost:6363/api/trends/{id}
+     */
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteTrend(@PathVariable Long id) {
         boolean deleted = trendService.deleteTrend(id);
@@ -62,30 +97,65 @@ public class TrendController {
         }
     }
 
+    /**
+     * Fetches trends associated with a specific category.
+     *
+     * @param categoryId the ID of the category
+     * @return a list of trends by the specified category
+     * @apiEndpoint GET http://localhost:6363/api/trends/category/{categoryId}
+     */
     @GetMapping("/category/{categoryId}")
     public ResponseEntity<List<Trend>> getTrendsByCategory(@PathVariable Long categoryId) {
         List<Trend> trends = trendService.getTrendsByCategory(categoryId);
         return new ResponseEntity<>(trends, HttpStatus.OK);
     }
 
+    /**
+     * Fetches trends designed by a specific designer.
+     *
+     * @param designerId the ID of the designer
+     * @return a list of trends by the specified designer
+     * @apiEndpoint GET http://localhost:6363/api/trends/designer/{designerId}
+     */
     @GetMapping("/designer/{designerId}")
     public ResponseEntity<List<Trend>> getTrendsByDesigner(@PathVariable Long designerId) {
         List<Trend> trends = trendService.getTrendsByDesigner(designerId);
         return new ResponseEntity<>(trends, HttpStatus.OK);
     }
 
+    /**
+     * Fetches trends from a specific location.
+     *
+     * @param location the location name
+     * @return a list of trends from the specified location
+     * @apiEndpoint GET http://localhost:6363/api/trends/location/{location}
+     */
     @GetMapping("/location/{location}")
     public ResponseEntity<List<Trend>> getTrendsByLocation(@PathVariable String location) {
         List<Trend> trends = trendService.getTrendsByLocation(location);
         return new ResponseEntity<>(trends, HttpStatus.OK);
     }
 
+    /**
+     * Fetches trends from a specific season.
+     *
+     * @param season the season name
+     * @return a list of trends from the specified season
+     * @apiEndpoint GET http://localhost:6363/api/trends/season/{season}
+     */
     @GetMapping("/season/{season}")
     public ResponseEntity<List<Trend>> getTrendsBySeason(@PathVariable String season) {
         List<Trend> trends = trendService.getTrendsBySeason(season);
         return new ResponseEntity<>(trends, HttpStatus.OK);
     }
 
+    /**
+     * Retrieves the popularity score of a specific trend.
+     *
+     * @param id the ID of the trend
+     * @return the popularity score of the trend, or 404 if not found
+     * @apiEndpoint GET http://localhost:6363/api/trends/{id}/popularityScore
+     */
     @GetMapping("/{id}/popularityScore")
     public ResponseEntity<Integer> getTrendPopularityScore(@PathVariable Long id) {
         Integer popularityScore = trendService.getTrendPopularityScore(id);
@@ -96,12 +166,28 @@ public class TrendController {
         }
     }
 
+    /**
+     * Records the popularity score for a specific trend.
+     *
+     * @param id the ID of the trend
+     * @param popularityScore the popularity score to be recorded
+     * @return a confirmation of the recording
+     * @apiEndpoint POST http://localhost:6363/api/trends/{id}/recordPopularityScore
+     */
     @PostMapping("/{id}/recordPopularityScore")
     public ResponseEntity<Void> recordPopularityScore(@PathVariable Long id, @RequestParam int popularityScore) {
         trendService.recordPopularityScore(id, popularityScore);
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
+    /**
+     * Associates a trend with a category.
+     *
+     * @param trendId the ID of the trend
+     * @param categoryId the ID of the category
+     * @return a confirmation of the association
+     * @apiEndpoint POST http://localhost:6363/api/trends/{trendId}/categories/{categoryId}
+     */
     @PostMapping("/{trendId}/categories/{categoryId}")
     public ResponseEntity<Void> associateTrendWithCategory(@PathVariable Long trendId, @PathVariable Long categoryId) {
         boolean associated = trendService.associateTrendWithCategory(trendId, categoryId);
@@ -112,6 +198,14 @@ public class TrendController {
         }
     }
 
+    /**
+     * Dissociates a trend from a category.
+     *
+     * @param trendId the ID of the trend
+     * @param categoryId the ID of the category
+     * @return a confirmation of the dissociation
+     * @apiEndpoint DELETE http://localhost:6363/api/trends/{trendId}/categories/{categoryId}
+     */
     @DeleteMapping("/{trendId}/categories/{categoryId}")
     public ResponseEntity<Void> dissociateTrendFromCategory(@PathVariable Long trendId, @PathVariable Long categoryId) {
         boolean dissociated = trendService.dissociateTrendFromCategory(trendId, categoryId);
@@ -121,5 +215,5 @@ public class TrendController {
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
     }
-
 }
+
