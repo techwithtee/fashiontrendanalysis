f2b61e523265548601ded0e0264949b20323cb31
TEE <78857687+techwithtee@users.noreply.github.com>
2023-09-27 21:46:12 +0100

    Updates to Visualtions

--- src/main/resources/static/category-analytics.js
+++ src/main/resources/static/category-analytics.js
@@ -1,16 +1,73 @@
-// Define a global variable to store category data
-let allCategoriesData = [];
+// Wait for the DOM to be ready
+document.addEventListener("DOMContentLoaded", function () {
+    // Fetch data from your Spring MVC backend for getCategoriesByTrend
+    fetch('/api/categories/trend/{trendId} ') // Replace {trendId} with an actual trend ID
+        .then(response => response.json())
+        .then(data => {
+            // Process and use the data here
+            console.log(data); // You can log it to see the retrieved data
 
-// Function to create a bar chart
-function createBarChart(chartId, labels, data, chartTitle) {
-    const ctx = document.getElementById(chartId).getContext('2d');
+            // Create a visualization (e.g., bar chart) for getCategoriesByTrend
+            createBarChartForCategoriesByTrend(data);
+        })
+        .catch(error => {
+            console.error('Error fetching data for getCategoriesByTrend:', error);
+        });
+
+    // Fetch data from your Spring MVC backend for getCategoriesByProduct
+    fetch('/api/categories/product/{productId}') // Replace {productId} with an actual product ID
+        .then(response => response.json())
+        .then(data => {
+            // Process and use the data here
+            console.log(data); // You can log it to see the retrieved data
+
+            // Create a visualization (e.g., pie chart) for getCategoriesByProduct
+            createPieChartForCategoriesByProduct(data);
+        })
+        .catch(error => {
+            console.error('Error fetching data for getCategoriesByProduct:', error);
+        });
+
+    // Fetch data from your Spring MVC backend for getCategoryPopularityForSeason
+    fetch('/api/categories/{categoryId}/popularity/{season}') // Replace {categoryId} and {season} with actual values
+        .then(response => response.json())
+        .then(data => {
+            // Process and use the data here
+            console.log(data); // You can log it to see the retrieved data
+
+            // Create a visualization (e.g., line chart) for getCategoryPopularityForSeason
+            createLineChartForCategoryPopularity(data);
+        })
+        .catch(error => {
+            console.error('Error fetching data for getCategoryPopularityForSeason:', error);
+        });
+
+    // Fetch data from your Spring MVC backend for getAllCategoryPopularities
+    fetch('/api/categories/{categoryId}/all-popularities') // Replace {categoryId} with an actual category ID
+        .then(response => response.json())
+        .then(data => {
+            // Process and use the data here
+            console.log(data); // You can log it to see the retrieved data
+
+            // Create a visualization (e.g., bar chart) for getAllCategoryPopularities
+            createBarChartForAllCategoryPopularities(data);
+        })
+        .catch(error => {
+            console.error('Error fetching data for getAllCategoryPopularities:', error);
+        });
+});
+
+
+// Function to create a bar chart for getCategoriesByTrend
+function createBarChartForCategoriesByTrend(data) {
+    const ctx = document.getElementById('chartByTrend').getContext('2d');
     new Chart(ctx, {
         type: 'bar',
         data: {
-            labels: labels,
+            labels: data.map(item => item.categoryName), // Use appropriate property names
             datasets: [{
-                label: chartTitle,
-                data: data,
+                label: 'Category Count',
+                data: data.map(item => item.categoryCount), // Use appropriate property names
                 backgroundColor: 'rgba(75, 192, 192, 0.2)',
                 borderColor: 'rgba(75, 192, 192, 1)',
                 borderWidth: 1
@@ -26,21 +83,73 @@ function createBarChart(chartId, labels, data, chartTitle) {
     });
 }
 
-// Function to fetch category popularity data
-function fetchCategoryPopularities() {
-    fetch('/{categoryId}/all-popularities')
-        .then(response => response.json())
-        .then(data => {
-            // Data has been fetched, you can use it for visualization
-            allCategoriesData = data;
-
-            // Now, create or update your chart
-            createBarChart('categoryPopularityChart', ['Category 1', 'Category 2', 'Category 3', 'Category 4', 'Category 5'], allCategoriesData, 'Category Popularity');
-        })
-        .catch(error => {
-            console.error('Error fetching data:', error);
-        });
+// Function to create a pie chart for getCategoriesByProduct
+function createPieChartForCategoriesByProduct(data) {
+    const ctx = document.getElementById('chartByProduct').getContext('2d');
+    new Chart(ctx, {
+        type: 'pie',
+        data: {
+            labels: data.map(item => item.categoryName), // Use appropriate property names
+            datasets: [{
+                data: data.map(item => item.categoryCount), // Use appropriate property names
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    // Add more colors as needed
+                ]
+            }]
+        }
+    });
 }
 
-// Call the function to fetch data when your page loads or as needed
-fetchCategoryPopularities();
+// Function to create a line chart for getCategoryPopularityForSeason
+function createLineChartForCategoryPopularity(data) {
+    const ctx = document.getElementById('chartCategoryPopularity').getContext('2d');
+    new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: data.map(item => item.season), // Use appropriate property names
+            datasets: [{
+                label: 'Popularity Score',
+                data: data.map(item => item.popularityScore), // Use appropriate property names
+                borderColor: 'rgba(75, 192, 192, 1)',
+                borderWidth: 1,
+                fill: false
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+}
+
+// Function to create a bar chart for getAllCategoryPopularities
+function createBarChartForAllCategoryPopularities(data) {
+    const ctx = document.getElementById('chartAllPopularities').getContext('2d');
+    new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: data.map(item => item.season), // Use appropriate property names
+            datasets: [{
+                label: 'Popularity Scores',
+                data: data.map(item => item.popularityScore), // Use appropriate property names
+                backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                borderColor: 'rgba(75, 192, 192, 1)',
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+}
+
